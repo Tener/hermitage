327543bfcd3334d675acbb396caca8d73293e9b3
Krzysztof Skrzętnicki <gtener@gmail.com>
2012-03-08 18:36:52 +0100

    big update

--- yesod-test.hs
+++ yesod-test.hs
@@ -8,8 +8,9 @@ import Control.Applicative
 import Data.Text
 import GHC.Generics
 import Data.Aeson
-
-
+import Control.Concurrent
+import Control.Monad
+import Data.Conduit
 -- Define our entities as usual
 
 -- data ProblemStatus = PS_Fresh | PS_Running | PS_Finished deriving (Show, Read, Eq)
@@ -18,10 +19,10 @@ share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
 Problem
     description Text
     files Text
-    status Text
 Submission
     language String
     text String
+    status Text
     problemId ProblemId
 |]
 
@@ -37,92 +38,105 @@ instance FromJSON Submission
 -- We keep our connection pool in the foundation. At program initialization, we
 -- create our initial pool, and each time we need to perform an action we check
 -- out a single connection from the pool.
-data PersistTest = PersistTest ConnectionPool
+data Hermitage = Hermitage ConnectionPool
 
--- We'll create a single route, to access a person. It's a very common
--- occurrence to use an Id type in routes.
-mkYesod "PersistTest" [parseRoutes|
+mkYesod "Hermitage" [parseRoutes|
 /problem/add/ ProblemAddR POST
 /problem/edit/#ProblemId ProblemEditR POST
 /problem/delete/#ProblemId ProblemDeleteR POST
 /problem/query/#ProblemId ProblemQueryR GET
-|]
--- /submission/add/ SubmissionAddR POST
--- /submission/edit/#SubmissionId SubmissionEditR POST
--- /submission/delete/#SubmissionId SubmissionDeleteR DELETE
--- /submission/query/#SubmissionId SubmissionQueryR GET
+/submission/add/ SubmissionAddR POST
+/submission/edit/#SubmissionId SubmissionEditR POST
+/submission/delete/#SubmissionId SubmissionDeleteR POST
+/submission/query/#SubmissionId SubmissionQueryR GET
 -- |]
 
 -- Nothing special here
-instance Yesod PersistTest
+instance Yesod Hermitage
 
-instance RenderMessage PersistTest FormMessage where
+instance RenderMessage Hermitage FormMessage where
     renderMessage _ _ = defaultFormMessage
 
 
 -- Now we need to define a YesodPersist instance, which will keep track of
 -- which backend we're using and how to run an action.
-instance YesodPersist PersistTest where
-    type YesodPersistBackend PersistTest = SqlPersist
+instance YesodPersist Hermitage where
+    type YesodPersistBackend Hermitage = SqlPersist
 
     runDB action = do
-        PersistTest pool <- getYesod
+        Hermitage pool <- getYesod
         runSqlPool action pool
-
-
--- postProblemAddR :: Handler RepPlain
--- postProblemAddR = do
---   person <- runInputPost $ Problem
---                              <$> ireq textField "description"
---                              <*> ireq textField "files"
---                              <*> ireq textField "status"
---   pid <- runDB $ insert person
---   return $ RepPlain $ toContent $ show (pid,person)
 
 postProblemAddR :: Handler RepJson
 postProblemAddR = do
-  (problem :: Problem) <- runInputPost $ Problem
-                             <$> ireq textField "description"
-                             <*> ireq textField "files"
-                             <*> ireq textField "status"
+  (problem :: Problem) <- parseJsonBody_
   pid <- runDB $ insert problem
-  jsonToRepJson (Data.Aeson.object [("PID" .= pid)])
+  jsonToRepJson (Data.Aeson.object [("PID" .= pid)], problem)
 
-postProblemEditR :: ProblemId -> Handler RepPlain
+postProblemEditR :: ProblemId -> Handler RepJson
 postProblemEditR pid = do
-  _ <- runDB $ get404 pid
-  problem <- runInputPost $ Problem
-                             <$> ireq textField "description"
-                             <*> ireq textField "files"
-                             <*> ireq textField "status"
+  runDB $ get404 pid
+  (problem :: Problem) <- parseJsonBody_
   runDB $ Database.Persist.Sqlite.replace pid problem
-  return $ RepPlain $ toContent $ show (pid,problem)
+  jsonToRepJson (Data.Aeson.object [("PID" .= pid)], problem)
 
-postProblemDeleteR :: ProblemId -> Handler RepPlain
+postProblemDeleteR :: ProblemId -> Handler RepJson
 postProblemDeleteR pid = do
   _ <- runDB $ get404 pid
   runDB $ delete pid
-  return $ RepPlain $ toContent $ show "Done."
+  jsonToRepJson ("Done." :: Text)
 
 getProblemQueryR :: ProblemId -> Handler RepJson
 getProblemQueryR pid = do
   pr <- runDB $ get404 pid
   jsonToRepJson pr
-  
 
--- We'll just return the show value of a problem, or a 404 if the Problem doesn't
--- exist.
--- getProblemR :: ProblemId -> Handler RepPlain
--- getProblemR problemId = do
---     problem <- runDB $ get404 problemId
---     return $ RepPlain $ toContent $ show problem
+postSubmissionAddR :: Handler RepJson
+postSubmissionAddR = do
+  (submission :: Submission) <- parseJsonBody_
+  sid <- runDB $ insert submission
+  jsonToRepJson (Data.Aeson.object [("SID" .= sid)], submission)
+
+postSubmissionEditR :: SubmissionId -> Handler RepJson
+postSubmissionEditR sid = do
+  runDB $ get404 sid
+  (submission :: Submission) <- parseJsonBody_
+  runDB $ Database.Persist.Sqlite.replace sid submission
+  jsonToRepJson (Data.Aeson.object [("SID" .= sid)], submission)
+
+postSubmissionDeleteR :: SubmissionId -> Handler RepJson
+postSubmissionDeleteR sid = do
+  _ <- runDB $ get404 sid
+  runDB $ delete sid
+  jsonToRepJson ("Done." :: Text)
+
+getSubmissionQueryR :: SubmissionId -> Handler RepJson
+getSubmissionQueryR sid = do
+  pr <- runDB $ get404 sid
+  jsonToRepJson pr
+
 
 openConnectionCount :: Int
 openConnectionCount = 10
 
+watchdog pool = forever $ do
+  threadDelay (10^4)
+  keys <- runSqlPool (do
+                       subm <- selectFirst [SubmissionStatus ==. "fresh"] []
+                       case subm of
+                         Nothing -> return ()
+                         Just ent -> do
+                                   update (entityKey ent) [SubmissionStatus =. "processing"]
+                       return subm
+                     ) pool
+  print keys
+  
+
 main :: IO ()
 main = withSqlitePool "test.db3" openConnectionCount $ \pool -> do
     runSqlPool (runMigration migrateAll) pool
-    k <- runSqlPool (insert $ Problem "Michael" "" "fresh") pool
+    forkIO (watchdog pool)
+    k <- runSqlPool (insert $ Problem "Michał" "") pool
     print k
-    warpDebug 3000 $ PersistTest pool
+    print =<< runSqlPool (insert $ Submission "haskell" "main = return ()" "fresh" k) pool
+    warpDebug 3000 $ Hermitage pool
